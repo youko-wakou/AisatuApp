601d8b99102dc5ae198884078cfcf2fca1061eed
youko-wakou <appu2pudding@gmail.com>
2017-12-01 23:12:14 +0900

    log出力失敗段階

--- app/src/main/java/techacademy/wakou/youko/aisatuapp/MainActivity.java
+++ app/src/main/java/techacademy/wakou/youko/aisatuapp/MainActivity.java
@@ -37,16 +37,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                         Log.d("UI-PARTS", String.valueOf(hourOfDay) + ":" + String.valueOf(minute));
 
-//                        public void aisatu{
                             if(hourOfDay >= 2 && hourOfDay <= 9 && minute <= 59){
                                 mTextView.setText("おはよう");
-                            }else if(hourOfDay >= 10 && hourOfDay<= 17&& minute >= 59){
+                            }else if(hourOfDay >= 10 && hourOfDay<= 17&& minute <= 59){
                                 mTextView.setText("こんにちは");
-                            }else if(hourOfDay >= 18 && hourOfDay >= 1 && minute <= 59){
+                            }else if(hourOfDay >= 18 && hourOfDay <= 1 && minute <= 59){
                                 mTextView.setText("こんばんは");
                             }
-//                        }
-
                     }
                         },
 
